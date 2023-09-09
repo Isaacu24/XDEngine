395b0d25fb02b153ae7adcc19b81f2efc77cbfb2
Isaac <alwlsdydtjd1@gmail.com>
2023-09-09 23:18:41 +0900

    Add project files.

---
+++ XDEditor/GameProject/NewProject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XDEditor.GameProject
+{
+    public class ProjectTemplate
+    { 
+        public string ProjectType { get;  set; }
+        public string ProjectFile { get; set; }
+    }
+
+    class NewProject : ViewwModeBase
+    {
+        private string name = "NewProject";
+        public string Name
+        { 
+            get => name;
+            set
+            { 
+                if (name != value)
+                {
+                    name = value;
+
+                    OnPropertyChanged(nameof(name));
+                }
+            }
+        }
+
+        private string path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\XDProject\";
+        public string Path
+        {
+            get => path;
+            set
+            {
+                if (path!= value)
+                {
+                    path = value;
+
+                    OnPropertyChanged(nameof(path));
+                }
+            }
+        }
+    }
+
+}
---
+++ XDEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XDEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialog.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialog : Window
+    {
+        public ProjectBrowserDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender == openProjectButton)
+            {
+                if(true == newProjectButton.IsChecked)
+                {
+                    newProjectButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(0);
+                }
+
+                newProjectButton.IsChecked = true;
+            }
+
+            else
+            {
+
+                if (true == openProjectButton.IsChecked)
+                {
+                    openProjectButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(-800, 0, 0, 0);
+                }
+
+                openProjectButton.IsChecked = true;
+            }
+        }
+
+        private void newProjectButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
