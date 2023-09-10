bd4aabaedbb5740e4d5b491b1ceffceefcc0d73e
Isaac <alwlsdydtjd1@gmail.com>
2023-09-10 22:29:40 +0900

    에디터 구현 시작

--- XDEditor/GameProject/NewProject.cs
+++ XDEditor/GameProject/NewProject.cs
@@ -1,47 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using XDEditor.Utilities;
 
 namespace XDEditor.GameProject
 {
+    [DataContract]
     public class ProjectTemplate
-    { 
+    {
+        [DataMember]
         public string ProjectType { get;  set; }
+
+        [DataMember]
         public string ProjectFile { get; set; }
+
+        [DataMember]
+        public List<string> Folders { get; set; }
+
+        public byte[] Icon { get; set; }
+        
+        public string IconFilePath { get; set; }
+
+        public byte[] Screenshot { get; set; }
+
+        public string ScreenshotFilePath { get; set; }
+
+        public string ProjectFilePath { get; set; }
+
     }
 
     class NewProject : ViewwModeBase
     {
-        private string name = "NewProject";
-        public string Name
+        // TODO: get the path from the installation location.
+        private readonly string templatepath = @"..\..\XDEditor\ProjectTemplates";
+        private string projectName = "NewProject";
+
+        public string ProjectName
         { 
-            get => name;
+            get => projectName;
             set
             { 
-                if (name != value)
+                if (projectName != value)
                 {
-                    name = value;
+                    projectName = value;
 
-                    OnPropertyChanged(nameof(name));
+                    OnPropertyChanged(nameof(projectName));
                 }
             }
         }
 
-        private string path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\XDProject\";
-        public string Path
+        private string projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\XDProject\";
+        public string ProjectPath
         {
-            get => path;
+            get => projectPath;
             set
             {
-                if (path!= value)
+                if (projectPath!= value)
                 {
-                    path = value;
+                    projectPath = value;
 
-                    OnPropertyChanged(nameof(path));
+                    OnPropertyChanged(nameof(projectPath));
                 }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> projectTemplates = new ObservableCollection<ProjectTemplate>();
+
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates
+        {
+            get;
+        }
+
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(projectTemplates);
+
+            try
+            {
+                var templateFiles = Directory.GetFiles(templatepath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+
+                foreach (var file in templateFiles)
+                {
+                    //Read template files.
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+
+                    projectTemplates.Add(template);
+
+                    //Write template files. (not use.)
+                    //var template = new ProjectTemplate()
+                    //{
+                    //    ProjectType = "Empty Project",
+                    //    ProjectFile = "project.xd",
+                    //    Folders = new List<string> { ".Xd", "Content", "GameCode" }
+                    //};
+
+                    //Serializer.ToFile(template, file);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // TODO: leg error
             }
         }
     }
--- XDEditor/GameProject/NewProjectView.xaml.cs
+++ XDEditor/GameProject/NewProjectView.xaml.cs
@@ -19,7 +19,7 @@ namespace XDEditor.GameProject
     /// Interaction logic for NewProjectView.xaml
     /// </summary>
     public partial class NewProjectView : UserControl
-    {
+    {   
         public NewProjectView()
         {
             InitializeComponent();
